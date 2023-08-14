8e936be03c3c5935ce3e9fde00922445dd9abec9
ruchithahoney <ruchithahoney1@gmail.com>
2023-08-13 20:20:50 -0400

    fix: add main files

---
+++ app.py
@@ -0,0 +1,31 @@
+from transformers import pipeline
+import gradio as gr
+
+# Load the text classification model
+fraud_detector = pipeline("text-classification")
+
+def fraud_detection(transaction_details):
+    prediction = fraud_detector(transaction_details)[0]
+    label = prediction['label']
+    confidence = prediction['score']
+    #print(label, confidence)
+    
+    if label == 'NEGATIVE':
+        return f"Fraudulent. Confidence: {confidence:.2f}"
+    else:
+        return f"Legitimate. Confidence: {confidence:.2f}"
+
+
+
+iface = gr.Interface(fn=fraud_detection, inputs="text", outputs="text", layout="vertical",
+                     title="Fraud Detection System",
+                     description="Enter transaction details to predict whether it's fraudulent or legitimate.",
+                     examples=[
+                         ["Transfer of $300 to unknown account"],
+                         ["Purchase of groceries at a local store"],
+                         ["Unauthorized access to account"]
+                     ],
+                     allow_flagging="never"
+                     )
+
+iface.launch()
